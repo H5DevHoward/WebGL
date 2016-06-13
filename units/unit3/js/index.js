708693eed3ce3764ed80e038b1be0c6aa105890a
h5devhoward <h5devhoward@gmail.com>
2016-06-13 09:21:03 +0800

    update

--- units/unit3/js/index.js
+++ units/unit3/js/index.js
@@ -16,7 +16,7 @@ var FSHADER_SOURCE =
     'precision mediump float;\n' +
     'uniform vec4 u_FragColor;\n' + // uniform变量
     'void main() {\n' +
-    ' gl_FragColor = u_FragColor;\n' + // 设置颜色
+    ' gl_FragColor = vec4(1.0, 0.0, 0.0, 1);\n' + // 设置颜色
     '}\n';
 
 function main() {
@@ -34,6 +34,14 @@ function main() {
         return;
     }
 
+    // 设置顶点位置
+    var n = initVertexBuffers(gl);
+    if(n < 0) {
+        console.log('Failed to set the position of the vertices');
+        return;
+    }
+
+    /*
     // 获取attribute变量的存储位置
     var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
     var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
@@ -52,7 +60,7 @@ function main() {
     }
 
     // 将顶点位置传输给attribute变量
-    gl.vertexAttrib1f(a_PointSize, 5.0);
+    gl.vertexAttrib1f(a_PointSize, 10.0);
     //
     // var position = new Float32Array([0.0, 0.5, 0.0, 1.0]);
     // gl.vertexAttrib4fv(a_Position, position);
@@ -60,55 +68,47 @@ function main() {
     canvas.onmousedown = function(ev) {
         click(ev, gl, canvas, a_Position, u_FragColor);
     };
+    */
 
 
     gl.clearColor(0.0, 1.0, 1.0, 0.4);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    // 绘制一个点
-    // gl.drawArrays(gl.POINTS, 0, 1);
+    // 绘制三个点
+    gl.drawArrays(gl.POINTS, 0, n);
 }
 
-var g_points = []; // 鼠标点击位置数组
-var g_colors = []; // 存储点颜色的数组
+function initVertexBuffers(gl) {
+    var vertices = new Float32Array([
+        0.0, 0.5, -0.5, -0.5, 0.5, -0.5
+    ]);
 
-function click(ev, gl, canvas, a_Position, u_FragColor) {
-    var x = ev.clientX;
-    var y = ev.clientY;
-    var rect = ev.target.getBoundingClientRect();
+    var n = 3; // 点的个数
 
-    x = ((x-rect.left) - canvas.width/2)/(canvas.width/2);
-    y = (canvas.height/2 - (y-rect.top))/(canvas.height/2);
-
-    // 将坐标存储到 g_points 数组中
-    // g_points.push(x);
-    // g_points.push(y);
-    g_points.push([x, y]);
-
-    // 将点的颜色存储到g_colors数组中
-    if(x >= 0.0 && y >= 0.0) { // 第一象限
-        g_colors.push([1.0, 0.0, 0.0, 1.0]);
-    }else if(x < 0.0 && y< 0.0) { // 第三象限
-        g_colors.push([0.0, 1.0, 0.0, 1.0]);
-    }else {
-        g_colors.push([1.0, 1.0, 1.0, 1.0]);
+    // 创建缓冲区对象
+    var vertexBuffer = gl.createBuffer();
+    if(!vertexBuffer) {
+        console.log('Failed to create the buffer object');
+        return -1;
     }
 
-    // 清除 <canvas>
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    // 将缓冲区对象绑定到目标
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 
-    var len = g_points.length;
-    for(var i=0;i<len;++i) {
-        var xy = g_points[i];
-        var rgba = g_colors[i];
+    // 向缓冲区对象中写入数据
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
 
-        // 将点的位置传递到变量中 a_Position
-        gl.vertexAttrib2f(a_Position, xy[0], xy[1]);
-
-        // 将点的颜色传输到 u_FragColor 变量中
-        gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-
-        // 绘制点
-        gl.drawArrays(gl.POINTS, 0, 1);
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if(a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position');
+        return;
     }
+
+    // 将缓冲区对象分配给a_Position变量
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+
+    // 链接a_Position 变量与分配给他的缓冲区对象
+    gl.enableVertexAttribArray(a_Position);
+
+    return n;
 }
